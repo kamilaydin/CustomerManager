f5d60a24322129faa18414946801f4da45becd31
kamilaydin <44448054+kamilaydin@users.noreply.github.com>
2022-12-27 16:27:11 +0300

    Update CustomersController.cs

--- CustomerAPI/Controllers/CustomersController.cs
+++ CustomerAPI/Controllers/CustomersController.cs
@@ -45,50 +45,12 @@ namespace CustomerAPI.Controllers
                 .Where(customer => customer.Id == id)
                 .FirstOrDefault();
 
-            #region MVC Json POST İşlemi
-            //Customer customer = new Customer();
-            //customer.Name = "Yakup";
-            //customer.Surname = "Yılmaz";
-            //customer.Email = "abdurrahman@hotmail.com";
-            //customer.Gender = 'm';
-            //customer.SocialSecurityNumber = "147866556";
-            //customer.Addresses = new List<Address>() {  new Address() { City = "Şanlıurfa", Country = "Türkiye", PostalCode = "06000", Street = "Test" } };
-            //customer.Phones = new List<Phone>() { new Phone() { Number = "789456244", PhoneType = PhoneType.Business }, new Phone() { Number = "789456244", PhoneType = PhoneType.Home } };
-
-
-            //string jsonString = JsonSerializer.Serialize<Customer>(customer);
-
-
-            //StringContent content = new StringContent(jsonString, System.Text.Encoding.UTF8, "application/json");
-            //string url = "https://localhost:44340/api/Customers/PostCustomer";
-            //HttpResponseMessage result = httpClient.PostAsync(url, content).Result; 
-            #endregion
+           
 
             return Ok(customer);
         }
 
-        // GET: api/Customers/5
-        //[HttpGet("{id}")]
-        //public async Task<ActionResult<Customer>> GetCustomer(int id)
-        //{
-
-        //    var customers = _context.Customers
-        //        .Include(customer => customer.Adresses)
-        //        .Include(customer=> customer.Phones)
-        //        .Where(customer => customer.CId == id)
-        //        .FirstOrDefault();
-
-        //    if (customers == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return customers;
-        //}
-
-        // PUT: api/Customers/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+       
         [HttpPut]
         public async Task<IActionResult> PutCustomer(Customer customer)
         {
@@ -109,25 +71,7 @@ namespace CustomerAPI.Controllers
         public IActionResult PostCustomer([FromBody] Customer customer)
         {
 
-            #region Test
-            //var customers = await _context.Customers
-            //                                .Include(customer => customer.Addresses)
-            //                                    .ThenInclude(Adress => Adress.City)
-            //                                    .Include(customer => customer.Phones)
-
-            //                                .Where(customer => customer.Id == customer.Id)
-            //                                .FirstOrDefaultAsync();
-            //Customer customer = new Customer();
-            //customer.Name = "Abdurrahman";
-            //customer.Surname = "Haliç";
-            //customer.Email = "abdurrahman@hotmail.com";
-            //customer.Gender = 'm';
-            //customer.SocialSecurityNumber = "147866556";
-            //customer.Addresses = new List<Address>() { new Address() { City = "Ankara", Country = "Türkiye", PostalCode = "06000", Street = "Kızılay" }, new Address() { City = "Adana", Country = "Türkiye", PostalCode = "06000", Street = "Abc" }, new Address() { City = "Eskişehir", Country = "Türkiye", PostalCode = "06000", Street = "Test" } };
-            //customer.Phones = new List<Phone>() { new Phone() { Number = "05478963214", PhoneType = PhoneType.Mobile }, new Phone() { Number = "05478963214", PhoneType = PhoneType.Business }, new Phone() { Number = "05478963214", PhoneType = PhoneType.Home } };
-
-            //var customers = await _context.Customers.Include(c => c.Addresses).Include(c => c.Phones).ToListAsync(); 
-            #endregion
+           
 
             _context.Customers.Add(customer);
             _context.SaveChanges();
@@ -139,9 +83,12 @@ namespace CustomerAPI.Controllers
             // Count the number of occurrences of each vowel in the name
             int aCount = 0;
             int eCount = 0;
+            int ıCount = 0;
             int iCount = 0;
             int oCount = 0;
+            int öCount = 0;
             int uCount = 0;
+            int üCount = 0;
 
             string controlName = name.ToLower();
 
@@ -155,20 +102,35 @@ namespace CustomerAPI.Controllers
                     case 'e':
                         eCount++;
                         break;
+
+                    case 'ı':
+                        ıCount++;
+                        break;
+
                     case 'i':
                         iCount++;
                         break;
                     case 'o':
                         oCount++;
                         break;
+
+                    case 'ö':
+                        öCount++;
+                        break;
+
                     case 'u':
                         uCount++;
                         break;
+
+                    case 'ü':
+                        üCount++;
+                        break;
+
                 }
             }
 
             // Return true if at least 3 of the same vowels are present
-            return (aCount >= 3 || eCount >= 3 || iCount >= 3 || oCount >= 3 || uCount >= 3);
+            return (aCount >= 3 || eCount >= 3 || iCount >= 3 || oCount >= 3 || uCount >= 3 || ıCount >= 3 || öCount >= 3 || üCount >= 3 );
         }
 
         // DELETE: api/Customers/5
